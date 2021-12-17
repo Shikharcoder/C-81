ac12b0cd49fbacbc6d50dc7fbad8930648a51a63
Shikharcoder <92843337+Shikharcoder@users.noreply.github.com>
2021-12-17 14:59:40 +0530

    Add files via upload

---
+++ C81main.js
@@ -0,0 +1,27 @@
+canvas = document.getElementById("My_canvas");
+ctx= canvas.getContext("2d");
+
+color= "black";
+
+ctx.beginPath();
+ctx.strokeStyle=color;
+ctx.lineWidth=2;
+ctx.arc(200, 200, 50, 0, 2*Math.PI);
+ctx.stroke();
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e){
+    mouse_x=e.clientX - canvas.offsetLeft;
+    mouse_y=e.clientY - canvas.offsetTop;
+    
+    console.log("x&y:"+mouse_x, mouse_y);
+    circle(mouse_x, mouse_y);
+}
+function circle(mouse_x, mouse_y){
+    ctx.beginPath();
+    ctx.strokeStyle=color;
+    ctx.lineWidth=2;
+    ctx.arc(mouse_x, mouse_y, 50, 0, 2*Math.PI);
+    ctx.stroke();
+}
